8556e256a8b70ff8c1db6484adffb4bb71a42595
Davide Moro <d.moro@campus.unimib.it>
2020-04-16 00:50:48 +0200

    "struttura basilare RecyclerView"

--- CinemHUB/app/src/main/java/com/example/cinemhub/RecyclerViewAdapter.java
+++ CinemHUB/app/src/main/java/com/example/cinemhub/RecyclerViewAdapter.java
@@ -1,13 +1,33 @@
 package com.example.cinemhub;
 
+import android.nfc.Tag;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class RecyclerViewAdapter {
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+
+    private static final String TAG = "RecyclerViewAdapter";
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
         ImageView imageView;
@@ -15,7 +35,8 @@ public class RecyclerViewAdapter {
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-            imageView = itemView.findViewById(R.id.)
+            imageView = itemView.findViewById(R.id.imageView);
+            textView = itemView.findViewById(); //non trova la text view mannaggia a DIO
         }
     }
 }
