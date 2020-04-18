406cc43b1efa9edb6d79bb2d4a251685a9ffe2c4
Davide Moro <d.moro@campus.unimib.it>
2020-04-18 15:01:00 +0200

    "procedo con struttura content_main"

---
+++ CinemHUB/app/src/main/java/com/example/cinemhub/schermata_prova.java
@@ -0,0 +1,14 @@
+package com.example.cinemhub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class schermata_prova extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.preferiti_overview_content);
+    }
+}
