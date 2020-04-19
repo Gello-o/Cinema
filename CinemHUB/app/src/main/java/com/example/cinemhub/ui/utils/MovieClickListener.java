0a5cf4a9e5027e0228b530314c55ac242e5f8429
Gello-o <gallizzi.uni@gmail.com>
2020-04-19 12:27:42 +0200

    Correzioni errori classi movie

--- CinemHUB/app/src/main/java/com/example/cinemhub/ui/utils/MovieClickListener.java
+++
@@ -1,9 +0,0 @@
-package com.example.cinemhub.ui.utils;
-
-import com.example.cinemhub.cinemhub.model.Movie;
-
-@SuppressWarnings("ALL")
-public interface MovieClickListener {
-    void onMovieClick(Movie movie);
-
-}
