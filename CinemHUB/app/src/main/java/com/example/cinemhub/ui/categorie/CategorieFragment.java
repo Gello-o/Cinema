e92d16b3bca362991d7bb9f57845056d72ccae3e
Gello-o <gallizzi.uni@gmail.com>
2020-04-19 11:37:51 +0200

    Aggiunta dipendenze api, correzione fragment e layout

--- CinemHUB/app/src/main/java/com/example/cinemhub/ui/categorie/CategorieFragment.java
+++ CinemHUB/app/src/main/java/com/example/cinemhub/ui/categorie/CategorieFragment.java
@@ -10,7 +10,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.example.cinemhub.R;
 
@@ -21,7 +21,7 @@ public class CategorieFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         categorieViewModel =
-                ViewModelProviders.of(this).get(CategorieViewModel.class);
+                new ViewModelProvider(this).get(CategorieViewModel.class);
         View root = inflater.inflate(R.layout.fragment_categorie, container, false);
         final TextView textView = root.findViewById(R.id.text_categorie);
         categorieViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
