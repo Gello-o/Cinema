a4bb7a07b97f06a92c9cb149cae6d6a8c923c3c6
Mattia Garotta <m.garotta4@campus.unimib.it>
2020-05-11 17:48:24 +0200

    aggiunta pagina ricerca

--- CinemHUB/app/src/main/java/com/example/cinemhub/ActivitySearch.java
+++ CinemHUB/app/src/main/java/com/example/cinemhub/ActivitySearch.java
@@ -3,11 +3,13 @@ package com.example.cinemhub;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class SettingsActivity extends Activity {
+
+public class ActivitySearch extends Activity {
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.settings);
+        setContentView(R.layout.fragment_search);
 
     }
 
