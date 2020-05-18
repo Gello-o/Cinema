90d6b49cb922b2f2015a2555e844a090da650a34
Moro <davide.moro@capgemini.com>
2020-05-18 16:09:41 +0200

    task asincrono che carica film up

--- CinemHUB/app/src/main/java/com/example/cinemhub/model/MoviesRepository.java
+++ CinemHUB/app/src/main/java/com/example/cinemhub/model/MoviesRepository.java
@@ -1,19 +1,15 @@
 package com.example.cinemhub.model;
 
-import android.text.method.MovementMethod;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.MutableLiveData;
-import androidx.paging.PagedList;
 
 import com.example.cinemhub.api.Client;
 import com.example.cinemhub.api.Service;
 
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
 
 
 import retrofit2.Call;
@@ -35,40 +31,54 @@ public class MoviesRepository {
         return instance;
     }
 
-    public void getMovies(String categoria, int pagina, MutableLiveData<PagedList<Movie>> moviesData) {
+    public void getMovies(String categoria, int pagina, MutableLiveData<List<Movie>> moviesData) {
         Service apiService = Client.getClient().create(Service.class);
         Call<MoviesResponse> call;
 
 
-            Log.d(TAG, "CHIAMATA " + pagina);
-            call = apiService.getTMDB(categoria, API_KEY, LANGUAGE, pagina);
+        Log.d(TAG, "CHIAMATA " + pagina);
+        call = apiService.getTMDB(categoria, API_KEY, LANGUAGE, pagina);
 
-            call.enqueue(new Callback<MoviesResponse>() {
+        call.enqueue(new Callback<MoviesResponse>() {
 
-                @Override
-                public void onResponse(@NonNull Call<MoviesResponse> call, @NonNull Response<MoviesResponse> response) {
-                    MoviesResponse moviesResponse = response.body();
-                    PagedList<Movie> movies = (PagedList<Movie>) moviesResponse.getResults();
+            @Override
+            public void onResponse(@NonNull Call<MoviesResponse> call, @NonNull Response<MoviesResponse> response) {
+                MoviesResponse moviesResponse = response.body();
+                List<Movie> movies = moviesResponse.getResults();
 
-                    if(moviesResponse == null){
-                        Log.d(TAG, "response null");
-                        //gestione risposta nulla
-                    }
-                    else if(movies == null){
-                        Log.d(TAG, "movies null");
-                        //gestione movies null
-                    }
+                if(moviesResponse == null){
+                    Log.d(TAG, "response null");
+                    //gestione risposta nulla
+                }
+                else if(movies == null){
+                    Log.d(TAG, "movies null");
                 }
 
-                @Override
-                public void onFailure(@NonNull Call<MoviesResponse> call, @NonNull Throwable t) {
-                    if (t.getMessage() != null)
-                        Log.d("Error", t.getMessage());
-                    else
-                        Log.d("Error", "qualcosa è andato storto");
+                if(moviesData.getValue() == null) {
+                    moviesData.setValue(movies);
+                    Log.d(TAG, "null zio pera");
+                    Log.d(TAG, "Pagina: " + pagina);
                 }
-            });
+
+                else {
+                    List<Movie> a = moviesData.getValue();
+                    a.addAll(movies);
+                    moviesData.setValue(a); //setValue
+
+                    Log.d(TAG, "not null zio pera");
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<MoviesResponse> call, @NonNull Throwable t) {
+                if (t.getMessage() != null)
+                    Log.d("Error", t.getMessage());
+                else
+                    Log.d("Error", "qualcosa è andato storto");
+            }
+        });
 
     }
+
 
 }
--- CinemHUB/app/src/main/java/com/example/cinemhub/ui/nuovi_arrivi/NuoviArriviViewModel.java
+++ CinemHUB/app/src/main/java/com/example/cinemhub/ui/nuovi_arrivi/NuoviArriviViewModel.java
@@ -1,68 +1,92 @@
 package com.example.cinemhub.ui.nuovi_arrivi;
 
-import android.app.ProgressDialog;
-import android.graphics.pdf.PdfRenderer;
-import android.os.Build;
-import android.text.method.MovementMethod;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Handler;
 import android.util.Log;
-import android.view.View;
-import android.widget.ProgressBar;
-import android.widget.Toast;
 
-import androidx.annotation.RequiresApi;
-import androidx.fragment.app.Fragment;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
-import androidx.paging.LivePagedListBuilder;
-import androidx.paging.PagedList;
 
-import com.example.cinemhub.MainActivity;
 import com.example.cinemhub.model.Movie;
 import com.example.cinemhub.model.MoviesRepository;
 
 
+import java.sql.Time;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class NuoviArriviViewModel extends ViewModel {
     private static final String TAG = "NuoviArriviViewModel";
-    private MutableLiveData<PagedList<Movie>> pagina;
-    private MutableLiveData<PagedList<Movie>> datoPaged;
-    MoviesRepository db;
-    
+    private MutableLiveData<List<Movie>> pagina;
+    boolean cinquePagine = false;
+    int index = 1;
+    MoviesRepository repo;
 
-    public MutableLiveData<PagedList<Movie>> getProssimeUscite() {
+    public MutableLiveData<List<Movie>> getProssimeUscite(Context context) {
+        repo = MoviesRepository.getInstance();
+
         if(pagina == null) {
-            db = MoviesRepository.getInstance();
             pagina = new MutableLiveData<>();
-            for(int i = 1; i < 3; i++){
-                db.getMovies("upcoming", i, pagina);
-            }
+            repo = MoviesRepository.getInstance();
+            repo.getMovies("upcoming", 1, pagina);
         }
         return pagina;
+
     }
 
-    /*
-public class ConcertTimeViewModel extends ViewModel {
-    private LiveData<PagedList<Concert>> concertList;
-    private DataSource<Date, Concert> mostRecentDataSource;
 
-    public ConcertTimeViewModel(Date firstConcertStartTime) {
-        ConcertTimeDataSourceFactory dataSourceFactory =
-                new ConcertTimeDataSourceFactory(firstConcertStartTime);
-        mostRecentDataSource = dataSourceFactory.create();
-        concertList = new LivePagedListBuilder<>(dataSourceFactory, 50)
-                .setFetchExecutor(myExecutor)
-                .build();
+    private class ShowMoreAsyncTask extends AsyncTask<Void, Void, LiveData<List<Movie>>> {
+
+        @Override
+        protected LiveData<List<Movie>> doInBackground(Void... voids) {
+            index++;
+            Log.d(TAG, "indice " + index);
+            repo.getMovies("upcoming", index, pagina);
+            return pagina;
+        }
     }
 
-    public void invalidateDataSource() {
-        mostRecentDataSource.invalidate();
+    private Timer timer;
+    private TimerTask timerTask;
+
+    public void setRepeatingAsyncTask(){
+        final Handler handler = new Handler();
+        timer = new Timer();
+        pagina.setValue(new ArrayList<>());
+        Log.d(TAG, "valore indice: " + index);
+
+        timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            ShowMoreAsyncTask task = new ShowMoreAsyncTask();
+                            task.execute();
+                        } catch (Exception e) {
+                            Log.d(TAG, "eccezione");
+                        }
+                    }
+                });
+            }
+        };
+        timer.schedule(timerTask, 0, 10*1000);
     }
-}
-*/
+
+    public void stopRepeatingTask(){
+        timer.cancel();
+        timer.purge();
+    }
+
+    public void nullifyTask(){
+        timer = null;
+        timerTask = null;
+    }
+
 }
